d96c806a3e13b85f98792fa717ba9c16f59155a9
LtGriff <ltgriff96@gmail.com>
2022-04-15 02:23:20 -0400

    push

---
+++ app.js
@@ -0,0 +1,37 @@
+var a = 10;
+var b = 20;
+console.log(a+b)
+console.log(b+c)
+
+
+var a = 10;
+var b = 20;
+console.log(a-b)
+console.log(b-c)
+
+
+var a = 10;
+var b = 20;
+console.log(a*b)
+console.log(b*c)
+
+
+var a = 10;
+var b = 20;
+console.log(a/b)
+console.log(b/c)
+
+
+var a = 10;
+var b = 20;
+console.log(a++b)
+console.log(b++c)
+
+
+var a = 10;
+var b = 20;
+console.log(a--b)
+console.log(b--c)
+
+
+
